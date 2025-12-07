5b2734fceece4c95dd2f719902ef3ab36e8f5b90
hawaameliaismatul-dev <hawaameliaismatul@gmail.com>
2025-12-07 22:41:45 +0700

    Update text halaman index

--- user/index.php
+++ user/index.php
@@ -5,7 +5,7 @@
 
 <div class="container">
     <div class="alert alert-info text-center">
-        <h4 style="margin-bottom: 0px;"><b>Selamat Datang!</b> Sistem Informasi Rental Kendaraan</h4>
+        <h4 style="margin-bottom: 0px;"><b>Selamat Datang!</b> Sistem Informasi Rental Kendaraan RPL Skanega</h4>
     </div>
 
     <div class="panel">
